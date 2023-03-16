a49d393a67a30aa60e746ccc6569e422550ac6d1
divineros3 <divineros3@icloud.com>
2023-03-15 21:07:56 -0600

    Basic JavaScript Projects Submission Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+document.write("Hello, World!");
+var A = "I\'m slowly catching up on my studies.";
+document.write(A);
+var B = "Concatenated" + " String"
+document.write(B)
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss");
+var Family = "The Godfreys", Dad = "Sean", Mom = "Divine", Daughter = "Pretty Girl", Son = "Luca";
+document.write(Dad);
+document.write(3+3);
+function My_First_function() {  //Defining a function and naming it
+    var str = "This is the button text!";   //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
